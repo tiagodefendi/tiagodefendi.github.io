629bee67cd6448568b5681e799e691375be9cc7b
Defendi <tiagodefendidasilva@gmail.com>
2025-05-02 13:41:05 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
     return (
         <footer className="bg-neutral-900 text-neutral-200 py-12 px-6">
             <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
-                <div className="space-y-3">
-                    <h3 className="text-lg font-semibold">Contact</h3>
+                <div className="space-y-2">
+                    <h3 className="text-lg font-semibold mt-3">Contact</h3>
                     <div className="flex flex-col gap-3 justify-start items-start">
                         <ContactButton
                             className="hover:underline"
@@ -21,7 +21,6 @@ const Footer = () => {
                             buttonName="+55 44 99890-5287"
                         />
                     </div>
-                    <p>Address: Rua Ipê, 490 - Araruna, Paraná</p>
                 </div>
 
                 <div className="space-y-3">
@@ -56,7 +55,7 @@ const Footer = () => {
                     <ul className="space-y-2">
                         <li><Link href="#" className="hover:underline">About Me</Link></li>
                         <li><Link href="#" className="hover:underline">Portfolio</Link></li>
-                        <li><Link href="https://linktr.ee/tiago_defendi" className="hover:underline">Link Tree</Link></li>
+                        <li><Link href="https://linktr.ee/tiago_defendi" target="_blank" rel="noopener noreferrer" className="hover:underline">Link Tree</Link></li>
                     </ul>
                 </div>
             </div>
